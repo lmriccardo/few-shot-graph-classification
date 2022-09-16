356911d48e39d9c19c825e81b45e12950e892650
lmriccardo <absintio098@gmail.com>
2022-09-16 19:02:31 +0200

    written new main and a Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,36 @@
-FROM python:3.10-slim-buster
+FROM ubuntu:22.10
+LABEL org.opencontainers.image.authors="riccardo.lamarca98@gmail.com"
 
-RUN groupadd -r fsgc && useradd -r -g fsgc-group fscg
-RUN chsh root -s /usr/sbin/nologin
+# Install python3.10.7 and python PIP
+RUN apt-get update && apt-get install -y build-essential
+RUN apt-get install -y python3.10 && ln -sf python3 /usr/bin/python
+RUN apt-get install -y python3-pip && ln -sf pip3 /usr/bin/pip
+
+# Install required packages
+RUN pip install --no-cache-dir -U pip
+RUN pip install --no-cache-dir https://download.pytorch.org/whl/cu116/torch-1.12.1%2Bcu116-cp310-cp310-linux_x86_64.whl
+RUN pip install --no-cache-dir torch-scatter     \
+                               torch-sparse      \
+                               torch-cluster     \
+                               torch-spline-conv \
+                               torch-geometric -f https://data.pyg.org/whl/torch-1.12.0+cu116.html
+RUN pip install --no-cache-dir numpy matplotlib plotly networkx sklearn wrapt rich
+
+# Add a new no-sudo user and disable root login
+RUN groupadd -r fsgc-group && useradd -r -g fsgc-group fsgc
+RUN chsh -s /usr/sbin/nologin root
+
+# Set some environment variables
+ENV LANG en_US.utf8
 ENV HOME /home/fscg
 
-WORKDIR /home/fscg/
+# Set the working directory and copy all required files
+COPY --chown=fsgc:fsgc-group ./models/ $HOME/app/models/
+COPY --chown=fsgc:fsgc-group ./src/ $HOME/app/src/
+COPY --chown=fsgc:fsgc-group ./data/ $HOME/app/data/
+WORKDIR $HOME/app/src/
+
+# Set the user to use inside the container
+USER fsgc:fsgc-group
+
+ENTRYPOINT ["uname", "-r"]
--- src/main.py
+++ src/main.py
@@ -8,117 +8,191 @@ from utils.utils import configure_logger
 from utils.trainer import Trainer
 from algorithms.asmaml.asmaml import AdaptiveStepMAML
 
-import paper
+import argparse
 import config
-import torch
 
 
-def asmaml():
-    torch.set_printoptions(edgeitems=config.EDGELIMIT_PRINT)
-    logger = configure_logger(file_logging=config.FILE_LOGGING, logging_path=config.LOGGING_PATH)
+def main() -> None:
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    dataset_name = config.DEFAULT_DATASET
+    # General arguments for the script
+    parser.add_argument('-p', '--path',      help="The path of the dataset",                   type=str,  default=config.DATA_PATH)
+    parser.add_argument('-n', '--name',      help="The name of the dataset",                   type=str,  default=config.DEFAULT_DATASET)
+    parser.add_argument('-d', '--device',    help="The device to use",                         type=str,  default=config.DEVICE)
+    parser.add_argument('-l', '--log-path',  help="The path where to log",                     type=str,  default=config.LOGGING_PATH)
+    parser.add_argument('-f', '--file-log',  help="If logging to file or not",                            default=config.FILE_LOGGING, action="store_true")
+    parser.add_argument('-s', '--save-path', help="The path where to save pre-trained models", type=str,  default=config.MODELS_SAVE_PATH)
+    parser.add_argument('-m', '--model',     help="The name of the model (sage or gcn)",       type=str,  default=config.MODEL_NAME)
+    parser.add_argument('--not-as-maml',     help="Use AS-MAML or not",                                   default=True, action="store_false")
+    parser.add_argument('--g-mixup',         help="Use G-Mixup or not",                                   default=False, action="store_true")
+    parser.add_argument('--flag',            help="Use FLAG or not",                                      default=False, action="store_true")
+    parser.add_argument('--m-evolve',        help="Use M-Evolve or not",                                  default=False, action="store_true")
+
+    # Configurations for the AS-MAML Model
+    parser.add_argument('--batch-size',    help="Dimension of a batch",                            type=int,   default=1)
+    parser.add_argument('--nhid',          help="Number of hidden dimension",                      type=int,   default=config.NHID)
+    parser.add_argument('--pool-ratio',    help="The pooling ratio",                               type=float, default=config.POOLING_RATIO)
+    parser.add_argument('--drop-ratio',    help="The dropout ratio",                               type=float, default=config.DROPOUT_RATIO)
+    parser.add_argument('--outer_lr',      help="Initial LR for the model",                        type=float, default=config.OUTER_LR)
+    parser.add_argument('--inner_lr',      help="Initial LR for the meta model",                   type=float, default=config.INNER_LR)
+    parser.add_argument('--stop_lr',       help="Initial LR for the Stop model",                   type=float, default=config.STOP_LR)
+    parser.add_argument('--w-decay',       help="The Weight Decay for optimizer",                  type=float, default=config.WEIGHT_DECAY)
+    parser.add_argument('--max-step',      help="The Max Step of the meta model",                  type=int,   default=config.MAX_STEP)
+    parser.add_argument('--min-step',      help="The Min Step of the meta model",                  type=int,   default=config.MIN_STEP)
+    parser.add_argument('--penality',      help="Step Penality for the RL model",                  type=float, default=config.STEP_PENALITY)
+    parser.add_argument('--train-shot',    help="The number of Shot per Training",                 type=int,   default=config.TRAIN_SHOT)
+    parser.add_argument('--val-shot',      help="The number of shot per Validation",               type=int,   default=config.VAL_SHOT)
+    parser.add_argument('--train-query',   help="The number of query per Training",                type=int,   default=config.TRAIN_QUERY)
+    parser.add_argument('--val-query',     help="The number of query per Validation",              type=int,   default=config.VAL_QUERY)
+    parser.add_argument('--train-way',     help="The number of way for Training",                  type=int,   default=config.TRAIN_WAY)
+    parser.add_argument('--test-way',      help="The number of way for Test and Val",              type=int,   default=config.TEST_WAY)
+    parser.add_argument('--val-episode',   help="The number of episode for Val",                   type=int,   default=config.VAL_EPISODE)
+    parser.add_argument('--train-episode', help="The number of episode for Training",              type=int,   default=config.TRAIN_EPISODE)
+    parser.add_argument('--batch-episode', help="The number of batch per episode",                 type=int,   default=config.BATCH_PER_EPISODES)
+    parser.add_argument('--epochs',        help="The total number of epochs",                      type=int,   default=config.EPOCHS)
+    parser.add_argument('--patience',      help="The patience",                                    type=int,   default=config.PATIENCE)
+    parser.add_argument('--grad-clip',     help="The clipping for the gradient",                   type=int,   default=config.GRAD_CLIP)
+    parser.add_argument('--scis',          help="The input dimension for the Stop Control model",  type=int,   default=config.STOP_CONTROL_INPUT_SIZE)
+    parser.add_argument('--schs',          help="The hidden dimension for the Stop Control model", type=int,   default=config.STOP_CONTROL_HIDDEN_SIZE)
+
+    # M-Evolve configurations
+    parser.add_argument('--beta',      help="The beta used in heuristics of M-Evolve",      type=float, default=config.BETA)
+    parser.add_argument('--n-fold',    help="The number of Fold for the nX-fol-validation", type=int,   default=config.N_FOLD)
+    parser.add_argument('--n-xval',    help="Number of Cross-fold Validation to run",       type=int,   default=config.N_CROSSVALIDATION)
+    parser.add_argument('--iters',     help="Number of iterations of M-Evolve",             type=int,   default=config.ITERATIONS)
+    parser.add_argument('--heuristic', help="The Heuristic to use",                         type=str,   default=config.HEURISTIC)
+    parser.add_argument('--lrts',      help="The label reliability step thresholds",        type=int,   default=config.LABEL_REL_THRESHOLD_STEPS)
+    parser.add_argument('--lrtb',      help="The beta used for approximation of the tanh",  type=int,   default=config.LABEL_REL_THRESHOLD_BETA)
+    parser.add_argument('--lrtss',     help="The label reliability threshold step size",    type=float, default=config.LABEL_REL_THRESHOLD_STEP_SIZE)
+
+    # FLAG configurations
+    parser.add_argument('--flag-m', help="The number of iterations of FLAG", type=int,   default=config.M)
+    parser.add_argument('--ass',    help="The attack step size",             type=float, default=config.ATTACK_STEP_SIZE)
+
+
+    args = parser.parse_args()
+    assert args.model in ["sage", "gcn"], f"Model name: {args.model} has not been implemented yet"
+    assert sum([args.use_mevolve, args.use_flag, args.use_gmixup]) < 2, "Cannot use more than one GDA technique at the same time"
+
+    configs = {
+        "data_path"     : args.path,
+        "data_name"     : args.name,
+        "device"        : args.device,
+        "log_path"      : args.log_path,
+        "file_log"      : args.file_log,
+        "save_path"     : args.save_path,
+        "model"         : args.model,
+        "use_asmaml"    : args.not_as_maml,
+        "use_gmixup"    : args.g_mixup,
+        "use_flag"      : args.flag,
+        "use_mevolve"   : args.m_evolve,
+        "batch_size"    : args.batch_size,
+        "nhid"          : args.nihd,
+        "pool_ratio"    : args.pool_ratio,
+        "drop_ratio"    : args.drop_ratio,
+        "outer_lr"      : args.outer_lr,
+        "innter_lr"     : args.inner_lr,
+        "stop_lr"       : args.stop_lr,
+        "weight_decay"  : args.w_decay,
+        "max_step"      : args.max_step,
+        "min_step"      : args.min_step,
+        "penalty"       : args.penalty,
+        "train_shot"    : args.train_shot,
+        "val_shot"      : args.val_shot,
+        "train_query"   : args.train_query,
+        "val_query"     : args.val_query,
+        "train_way"     : args.train_way,
+        "test_way"      : args.test_way,
+        "val_episode"   : args.val_episode,
+        "train_episode" : args.train_episode,
+        "batch_episode" : args.batch_episode,
+        "epochs"        : args.epochs,
+        "patience"      : args.patience,
+        "grad_clip"     : args.grad_clip,
+        "scis"          : args.scis,
+        "schs"          : args.schc,
+        "beta"          : args.beta,
+        "n_fold"        : args.n_fold,
+        "n_xval"        : args.n_xval,
+        "iters"         : args.iters,
+        "heuristic"     : args.heuristic,
+        "lrts"          : args.lrts,
+        "lrtb"          : args.lrtb,
+        "lrtss"         : args.lrtss,
+        "flag_m"        : args.flag_m,
+        "ass"           : args.ass
+    }
+
+
+    logger = configure_logger(file_logging=configs["file_log"], logging_path=configs["log_path"])
+    dataset_name = configs["data_name"]
     train_ds, test_ds, val_ds, _ = get_dataset(
-        download=config.DOWNLOAD_DATASET, 
-        data_dir=config.DATA_PATH, 
-        logger=logger,
+        download=False, 
+        data_dir=configs["data_path"], 
+        logger=logger, 
         dataset_name=dataset_name
     )
 
     logger.debug("--- Datasets ---")
-    print("\n- Train: ", train_ds, file=sys.stdout if not config.FILE_LOGGING else open(logger.handlers[1].baseFilename, mode="a"))
-    print("- Test : ", test_ds, file=sys.stdout if not config.FILE_LOGGING else open(logger.handlers[1].baseFilename, mode="a"))
-    print("- Validation: ", val_ds, file=sys.stdout if not config.FILE_LOGGING else open(logger.handlers[1].baseFilename, mode="a"))
-    print("\n", file=sys.stdout if not config.FILE_LOGGING else open(logger.handlers[1].baseFilename, mode="a"))
+    print("\n- Train: ", train_ds, file=sys.stdout if not configs["file_log"] else open(logger.handlers[1].baseFilename, mode="a"))
+    print("- Test : ", test_ds, file=sys.stdout if not configs["file_log"] else open(logger.handlers[1].baseFilename, mode="a"))
+    print("- Validation: ", val_ds, file=sys.stdout if not configs["file_log"] else open(logger.handlers[1].baseFilename, mode="a"))
+    print("\n", file=sys.stdout if not configs["file_log"] else open(logger.handlers[1].baseFilename, mode="a"))
 
-    print("\n- Train: ", train_ds)
-    print("- Test : ", test_ds)
-    print("- Validation: ", val_ds)
-    print()
-
-    logger.debug("--- Configurations ---")
-
-    configurations = ("\nDEVICE: {device}\n"                            +
-                      "DATASET NAME: {dataset_name}\n"                + 
-                      "TRAIN SUPPORT SIZE: {train_support_size}\n"    +
-                      "TRAIN QUERY SIZE: {train_query_size}\n"        +
-                      "VALIDATION SUPPORT SIZE: {val_support_size}\n" +
-                      "VALIDATION QUERY SIZE: {val_query_size}\n"     +
-                      "TEST SUPPORT SIZE: {test_support_size}\n"      +
-                      "TEST QUERY SIZE: {test_query_size}\n"          +
-                      "TRAIN EPISODE: {train_episode}\n"              +
-                      "VALIDATION EPISODE: {val_episode}\n"           +
-                      "NUMBER OF EPOCHS: {number_of_epochs}\n"        +
-                      "BATCH PER EPISODES: {batch_per_episodes}\n"
+    logger.debug("--- Configuration ---")
+    configurations = ("\nDEVICE: {device}\n"                             +
+                      "DATASET NAME: {dataset_name}\n"                   + 
+                      "USE ASMAML: {use_asmaml}\n"                       +
+                      "USE_GMIXUP : {use_gmixup}\n"                      +
+                      "USE_MEVOLVE : {use_mevolve}\n"                    + 
+                      "USE_FLAG : {use_flag}\n"                          + 
+                      "TRAIN SUPPORT SIZE: {train_support_size}\n"       +
+                      "TRAIN QUERY SIZE: {train_query_size}\n"           +
+                      "VALIDATION SUPPORT SIZE: {val_support_size}\n"    +
+                      "VALIDATION QUERY SIZE: {val_query_size}\n"        +
+                      "TEST SUPPORT SIZE: {test_support_size}\n"         +
+                      "TEST QUERY SIZE: {test_query_size}\n"             +
+                      "TRAIN EPISODE: {train_episode}\n"                 +
+                      "VALIDATION EPISODE: {val_episode}\n"              +
+                      "NUMBER OF EPOCHS: {number_of_epochs}\n"           +
+                      "BATCH PER EPISODES: {batch_per_episodes}\n"       + 
+                      "NUMBER OF FOLDS : {n_folds}\n"                    +
+                      "NUMBER OF CROSS-FOLD VALIDATION RUNS: {n_xval}\n" +
+                      "M-EVOLVE ITERATIONS : {m_iters}\n"                + 
+                      "M-EVOLVE HEURISTIC : {m_heu}\n"                   +
+                      "FLAG ITERATIONS : {flag_m}\n"                     
         ).format(
-            device=config.DEVICE, dataset_name=dataset_name,
-            train_support_size=f"{config.TRAIN_WAY} x {config.TRAIN_SHOT}",
-            train_query_size=f"{config.TRAIN_WAY} x {config.TRAIN_QUERY}",
-            val_support_size=f"{config.TEST_WAY} x {config.VAL_SHOT}",
-            val_query_size=f"{config.TEST_WAY} x {config.VAL_QUERY}",
-            test_support_size=f"{config.TEST_WAY} x {config.VAL_SHOT}",
-            test_query_size=f"{config.TEST_WAY} x {config.VAL_QUERY}",
-            train_episode=config.TRAIN_EPISODE, val_episode=config.VAL_EPISODE,
-            number_of_epochs=config.EPOCHS, batch_per_episodes=config.BATCH_PER_EPISODES
-        )
+            device=configs["device"], dataset_name=dataset_name,
+            use_asmaml=configs["use_asmaml"], use_gmixup=configs["use_gmixup"], 
+            use_flag=configs["use_flag"], use_mevolve=configs["use_mevolve"],
+            train_support_size="{} x {}".format(configs["train_way"], configs["train_shot"]),
+            train_query_size="{} x {}".format(configs["train_way"], configs["train_query"]),
+            val_support_size="{} x {}".format(configs["test_way"], configs["val_shot"]),
+            val_query_size="{} x {}".format(configs["test_way"], configs["val_query"]),
+            test_support_size="{} x {}".format(configs["test_way"], configs["val_shot"]),
+            test_query_size="{} x {}".format(configs["test_way"], configs["val_query"]),
+            train_episode=configs["train_episode"], val_episode=configs["val_episode"],
+            number_of_epochs=configs["epochs"], batch_per_episodes=configs["batch_episode"],
+            n_folds=configs["n_fold"] if configs["use_mevolve"] else "M-Evolve not used",
+            n_xval=configs["n_xval"] if configs["use_mevolve"] else "M-Evolve not used",
+            m_iters=configs["iters"] if configs["use_mevolve"] else "M-Evolve not used",
+            m_heu=configs["heuristic"] if configs["use_mevolve"] else "M-Evolve not used",
+            flag_m=configs["falg_m"] if configs["use_flag"] else "FLAG not used"
+    )
 
-    print(configurations, file=sys.stdout if not config.FILE_LOGGING else open(logger.handlers[1].baseFilename, mode="a"))
+    print(configurations, file=sys.stdout if not configs["file_log"] else open(logger.handlers[1].baseFilename, mode="a"))
 
-    # optimizer = ASMAMLTrainer(train_ds, val_ds, logger, 
-    #                           epochs=config.EPOCHS, 
-    #                           dataset_name=dataset_name,
-    #                           model_name="sage"
-    #             )
-            
-    # optimizer.train()
-
+    # Run the trainer
+    meta_model = AdaptiveStepMAML if configs["use_asmaml"] else None
+    save_suffix = "ASMAML_" if configs["use_asmaml"] else "_"
     optimizer = Trainer(
-        train_ds=train_ds, val_ds=val_ds, logger=logger,
-        model_name="sage", paper=False, epochs=config.EPOCHS, batch_size=1,
-        dataset_name=config.DEFAULT_DATASET, meta_model=AdaptiveStepMAML,
-        save_suffix="ASMAML_", use_mevolve=False, use_flag=False, use_gmixup=False
+        train_ds=train_ds, val_ds=val_ds, logger=logger, 
+        meta_model=meta_model, save_suffix=save_suffix,
+        **configs
     )
 
     optimizer.run()
 
-    # best_model_path = os.path.join(config.MODELS_SAVE_PATH, f"{dataset_name}_BestModel.pth")
-    # tester = Tester(test_ds, logger, best_model_path)
-    # tester.test()
 
-    # delete_data_folder(data_dir)
-
-
-def run_paper() -> None:
-    logger = configure_logger(file_logging=config.FILE_LOGGING, logging_path=config.LOGGING_PATH)
-    dataset = paper.GraphDataset(val=False)
-    val_dataset = paper.GraphDataset(val=True)
-    train_loader = paper.FewShotDataLoaderPaper(
-        dataset=dataset,
-        n_way=3,
-        k_shot=10,
-        n_query=15,
-        batch_size=1,
-        num_workers=4,
-        epoch_size=200
-    )
-
-    val_loader = paper.FewShotDataLoaderPaper(
-        dataset=val_dataset,
-        n_way=3,
-        k_shot=10,
-        n_query=15,
-        batch_size=1,
-        num_workers=4,
-        epoch_size=200
-    )
-
-    optimizer = ASMAMLTrainer(dataset, val_dataset, logger, epochs=config.EPOCHS, dataset_name=config.DEFAULT_DATASET, paper=True)
-    optimizer.train_dl = train_loader(0)
-    optimizer.val_dl   = val_loader(0)
-
-    optimizer.train()
-
-
-if __name__ == "__main__":
-    asmaml()
-    # run_paper()
+if __name__ == '__main__':
+	main()
